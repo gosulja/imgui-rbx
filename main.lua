64bd1b06186e116b9a189d4eeb7172f85d893f64
cassidy <111649405+wiIlow@users.noreply.github.com>
2023-04-09 21:18:19 +0100

    Working but only applys to one value, fiex i think

--- main.lua
+++ main.lua
@@ -808,10 +808,13 @@ function main:Begin(PROPS)
         local function updateColor(color, value)
             if color == "R" then
                 CurrentColor = Color3.fromRGB(value, CurrentColor.G, CurrentColor.B)
+                Color_ElementRedTextInput.Text = string.format("R:%s", value);
             elseif color == "G" then
                 CurrentColor = Color3.fromRGB(CurrentColor.R, value, CurrentColor.B)
+                Color_ElementBlueTextInput.Text = string.format("B:%s", value);
             elseif color == "B" then
                 CurrentColor = Color3.fromRGB(CurrentColor.R, CurrentColor.G, value)
+                Color_ElementBlueTextInput.Text = string.format("B:%s", value);
             end
             setColor()
         end
@@ -820,16 +823,16 @@ function main:Begin(PROPS)
             if colorButton:IsA("TextButton") then
                 colorButton.MouseButton1Down:Connect(function()
                     while UIS:IsMouseButtonPressed(Enum.UserInputType.MouseButton1) do
-                        local x = math.clamp(UIS:GetMouseLocation().X - colorButton.AbsolutePosition.X, 0, colorButton.AbsoluteSize.X)
-                        local value = math.floor(x / colorButton.AbsoluteSize.X * 255 + 0.5)
-                        local color = string.sub(colorButton.Name, 14, 14)
-                        updateColor(color, value)
+                        local r = math.floor(Color_ElementRedButtonInput.AbsolutePosition.X / Color_ElementRedButtonInput.AbsoluteSize.X * 255)
+                        local g = math.floor(Color_ElementGreenButtonInput.AbsolutePosition.X / Color_ElementGreenButtonInput.AbsoluteSize.X * 255)
+                        local b = math.floor(Color_ElementBlueButtonInput.AbsolutePosition.X / Color_ElementBlueButtonInput.AbsoluteSize.X * 255)
+                        CurrentColor = Color3.fromRGB(r, g, b)
+                        setColor()
                         task.wait()
                     end
                 end)
             end
         end
-
 
     end
 
